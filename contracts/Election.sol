10b4c6c0e9c104686f51df0b33433f702245d4f1
AmosAidoo <aidooamos20@gmail.com>
2019-03-29 21:39:31 -0700

    Added functionality for voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
         uint voteCount;
     }
 
+    //Associative array for ccounts that have voted
+    mapping (uint => bool) public voters;
+    
     //Associative array for candidates
     mapping(uint => Candidate) public candidates;
 
@@ -19,8 +22,22 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
+    //Add new candidate
     function addCandidate(string memory _name) private {
         candidateCount++;
         candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    }
+
+    //Vote Casting
+    function vote(uint _candidateId) public {
+        //Check if voter hasn't aleady voted
+        require(!voters[uint256(msg.sender)]);
+
+        //Check whether the candidateId is valid
+        require(_candidateId > 0 && _candidateId <= candidateCount);
+
+        voters[uint256(msg.sender)] = true;
+
+        candidates[_candidateId].voteCount++;
     }
 }
